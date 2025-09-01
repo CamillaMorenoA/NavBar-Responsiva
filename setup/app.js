972cf20cefb7590a1543b9e9fe28c3df27642d2b
Liberato <157623242+Liberatinho@users.noreply.github.com>
2025-09-01 14:44:51 -0300

    Tela do rock e rap feitas

--- setup/app.js
+++ setup/app.js
@@ -6,9 +6,36 @@
 
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const body = document.querySelector("p");
 
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show-links");
 });
 
+
+links.addEventListener("click", function (e) {
+  if (e.target.tagName === "A") {
+    e.preventDefault(); // impede abrir a página direto
+    const linkText = e.target.textContent.toLowerCase();
+
+    if (linkText === "rock") {
+      body.innerHTML = `
+        <iframe src="rock.html" width="100%" height="1000px" style="border:1px solid #ccc;"></iframe>
+      `;
+    }
+  }
+});
+
+links.addEventListener("click", function (e) {
+  if (e.target.tagName === "A") {
+    e.preventDefault(); // impede abrir a página direto
+    const linkText = e.target.textContent.toLowerCase();
+
+    if (linkText === "rap") {
+      body.innerHTML = `
+        <iframe src="rap.html" width="100%" height="1000px" style="border:1px solid #ccc;"></iframe>
+      `;
+    }
+  }
+});
 
